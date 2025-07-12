84ef640421e560c75da4835649d280377e5125e3
nyteksf <email.nytek@gmail.com>
2025-07-12 01:15:06 -0700

    Update contact info on AboutPage.jsx

--- covered-bridge-squeeze-page/src/pages/AboutPage.jsx
+++ covered-bridge-squeeze-page/src/pages/AboutPage.jsx
@@ -203,7 +203,7 @@ const AboutPage = () => {
                   >
                     <path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z" />
                   </svg>
-                  <span>(555) 123-4567</span>
+                  <span>(866) 610-LAND</span>
                 </div>
                 <div className="contact-item">
                   <svg
@@ -213,7 +213,9 @@ const AboutPage = () => {
                   >
                     <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" />
                   </svg>
-                  <span>info@coveredbridge.properties</span>
+                  <a href="mailto:joe@coveredbridge.properties">
+                    <span>joe@coveredbridge.properties</span>
+                  </a>
                 </div>
               </div>
             </section>
